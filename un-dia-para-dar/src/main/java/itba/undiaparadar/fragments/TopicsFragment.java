8447e67e56d45e87eefd3a7c1ae9d07d5903f934
MartuPuri <martinpurita@gmail.com>
2015-09-04 08:19:50 -0300

    Modify icons

--- un-dia-para-dar/src/main/java/itba/undiaparadar/fragments/TopicsFragment.java
+++ un-dia-para-dar/src/main/java/itba/undiaparadar/fragments/TopicsFragment.java
@@ -32,6 +32,7 @@ public class TopicsFragment extends Fragment implements TitleProvider {
     @Inject
     private TopicService topicService;
     private List<Topic> topics;
+    private MenuItem goToMap;
 
     public static Fragment newInstance() {
         return new TopicsFragment();
@@ -46,6 +47,7 @@ public class TopicsFragment extends Fragment implements TitleProvider {
                              Bundle savedInstanceState) {
         root = inflater.inflate(R.layout.fragment_topics, container, false);
         populateGrid();
+        setHasOptionsMenu(true);
 
         return root;
     }
@@ -62,9 +64,9 @@ public class TopicsFragment extends Fragment implements TitleProvider {
                 final ImageView img = (ImageView) view.findViewById(R.id.topic_img);
                 topicService.loadImageResId(adapter.getItem(position), img);
                 if (isAnyTopicSelected()) {
-                    setHasOptionsMenu(true);
+                    goToMap.setIcon(R.drawable.boton_corazon);
                 } else {
-                    setHasOptionsMenu(false);
+                    goToMap.setIcon(R.drawable.boton_corazon_gris);
                 }
             }
         });
@@ -77,23 +79,23 @@ public class TopicsFragment extends Fragment implements TitleProvider {
     private List<Topic> createTopics() {
         topics = new ArrayList<>();
 
-        topics.add(new Topic(R.drawable.adopcion_de_mascotas, getString(R.string.adopcion_mascotas), R.drawable.adopcion_de_mascotas));
-        topics.add(new Topic(R.drawable.asesor_directorio, getString(R.string.asesor_directorio), R.drawable.adopcion_de_mascotas));
-        topics.add(new Topic(R.drawable.donante_de_medula_osea, getString(R.string.donante_de_medula_osea), R.drawable.adopcion_de_mascotas));
-        topics.add(new Topic(R.drawable.donar_alimentos, getString(R.string.donar_alimentos), R.drawable.adopcion_de_mascotas));
-        topics.add(new Topic(R.drawable.donar_articulos_usados, getString(R.string.donar_articulos_usados), R.drawable.adopcion_de_mascotas));
-        topics.add(new Topic(R.drawable.donar_dinero, getString(R.string.donar_dinero), R.drawable.adopcion_de_mascotas));
-        topics.add(new Topic(R.drawable.donar_leche_materna, getString(R.string.donar_leche_materna), R.drawable.adopcion_de_mascotas));
-        topics.add(new Topic(R.drawable.donar_millas_aereas, getString(R.string.donar_millas_aereas), R.drawable.adopcion_de_mascotas));
-        topics.add(new Topic(R.drawable.donar_pelo, getString(R.string.donar_pelo), R.drawable.adopcion_de_mascotas));
-        topics.add(new Topic(R.drawable.donar_plaquetas, getString(R.string.donar_plaquetas), R.drawable.adopcion_de_mascotas));
-        topics.add(new Topic(R.drawable.donar_sangre, getString(R.string.donar_sangre), R.drawable.adopcion_de_mascotas));
-        topics.add(new Topic(R.drawable.donar_telefonos_moviles, getString(R.string.donar_telefonos_moviles), R.drawable.adopcion_de_mascotas));
-        topics.add(new Topic(R.drawable.formarse_para_ayudar, getString(R.string.formarse_para_ayudar), R.drawable.adopcion_de_mascotas));
-        topics.add(new Topic(R.drawable.plantar_arboles, getString(R.string.plantar_arboles), R.drawable.adopcion_de_mascotas));
-        topics.add(new Topic(R.drawable.publicacion_de_calle, getString(R.string.publicacion_de_calle), R.drawable.adopcion_de_mascotas));
-        topics.add(new Topic(R.drawable.reutilizar, getString(R.string.reutilizar), R.drawable.adopcion_de_mascotas));
-        topics.add(new Topic(R.drawable.voluntariado, getString(R.string.voluntariado), R.drawable.adopcion_de_mascotas));
+        topics.add(new Topic(R.drawable.adopcion_de_mascotas, getString(R.string.adopcion_mascotas), R.drawable.adopcion_de_mascotas_gris));
+        topics.add(new Topic(R.drawable.asesor_directorio, getString(R.string.asesor_directorio), R.drawable.asesor_directorio_gris));
+        topics.add(new Topic(R.drawable.donante_de_medula_osea, getString(R.string.donante_de_medula_osea), R.drawable.donante_de_medula_osea_gris));
+        topics.add(new Topic(R.drawable.donar_alimentos, getString(R.string.donar_alimentos), R.drawable.donar_alimentos_gris));
+        topics.add(new Topic(R.drawable.donar_articulos_usados, getString(R.string.donar_articulos_usados), R.drawable.donar_articulos_usados_gris));
+        topics.add(new Topic(R.drawable.donar_dinero, getString(R.string.donar_dinero), R.drawable.donar_dinero_gris));
+        topics.add(new Topic(R.drawable.donar_leche_materna, getString(R.string.donar_leche_materna), R.drawable.donar_leche_materna_gris));
+        topics.add(new Topic(R.drawable.donar_millas_aereas, getString(R.string.donar_millas_aereas), R.drawable.donar_millas_aereas_gris));
+        topics.add(new Topic(R.drawable.donar_pelo, getString(R.string.donar_pelo), R.drawable.donar_pelo_gris));
+        topics.add(new Topic(R.drawable.donar_plaquetas, getString(R.string.donar_plaquetas), R.drawable.donar_plaquetas_gris));
+        topics.add(new Topic(R.drawable.donar_sangre, getString(R.string.donar_sangre), R.drawable.donar_sangre_gris));
+        topics.add(new Topic(R.drawable.donar_telefonos_moviles, getString(R.string.donar_telefonos_moviles), R.drawable.donar_telefonos_moviles_gris));
+        topics.add(new Topic(R.drawable.formarse_para_ayudar, getString(R.string.formarse_para_ayudar), R.drawable.formarse_para_ayudar_gris));
+        topics.add(new Topic(R.drawable.plantar_arboles, getString(R.string.plantar_arboles), R.drawable.plantar_arboles_gris));
+        topics.add(new Topic(R.drawable.publicacion_de_calle, getString(R.string.publicacion_de_calle), R.drawable.publicacion_de_calle_gris));
+        topics.add(new Topic(R.drawable.reutilizar, getString(R.string.reutilizar), R.drawable.reutilizar_gris));
+        topics.add(new Topic(R.drawable.voluntariado, getString(R.string.voluntariado), R.drawable.voluntariado_gris));
 
         return topics;
     }
@@ -105,13 +107,10 @@ public class TopicsFragment extends Fragment implements TitleProvider {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(final MenuItem item) {
         if (item.getItemId() == R.id.go_to_map) {
             final List<Topic> selectedTopics = getSelectedTopics();
-            if (selectedTopics.isEmpty()) {
-                Toast.makeText(getActivity(), getString(R.string.empty_topics), Toast.LENGTH_SHORT)
-                        .show();
-            } else {
+            if (!selectedTopics.isEmpty()) {
                 getFragmentManager()
                         .beginTransaction()
                         .replace(R.id.main_content, MapFragment.newInstance())
@@ -126,6 +125,7 @@ public class TopicsFragment extends Fragment implements TitleProvider {
     @Override
     public void onCreateOptionsMenu(final Menu menu, final MenuInflater inflater) {
         inflater.inflate(R.menu.menu_topic, menu);
+        goToMap = menu.getItem(0);
     }
 
     private List<Topic> getSelectedTopics() {
