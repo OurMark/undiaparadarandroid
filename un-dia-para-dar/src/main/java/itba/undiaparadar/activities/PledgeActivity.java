fee823c0d0b90f018def7d43c11e00bd67161aba
Martin Purita <martin.purita@olx.com>
2016-01-22 16:47:19 -0300

    Change layout

--- un-dia-para-dar/src/main/java/itba/undiaparadar/activities/PledgeActivity.java
+++ un-dia-para-dar/src/main/java/itba/undiaparadar/activities/PledgeActivity.java
@@ -3,15 +3,13 @@ package itba.undiaparadar.activities;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import itba.undiaparadar.R;
 import itba.undiaparadar.UnDiaParaDarApplication;
 import itba.undiaparadar.model.PositiveAction;
 
-/**
- * Created by mpurita on 10/31/15.
- */
-public class PledgeActivity extends CircularRevealActivity {
+public class PledgeActivity extends AppCompatActivity {
 	private static final String POSITIVE_ACTION = "POSITIVE_ACTION";
 
 	public static Intent getIntent(final Context context, PositiveAction positiveAction) {
@@ -20,16 +18,10 @@ public class PledgeActivity extends CircularRevealActivity {
 		return intent;
 	}
 
-
-
 	@Override
 	protected void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		UnDiaParaDarApplication.injectMembers(this);
-	}
-
-	@Override
-	int getContentLayoutId() {
-		return R.layout.pledge_actiivty;
+		setContentView(R.layout.pledge_actiivty);
 	}
 }
