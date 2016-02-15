1fb2ba10a00edf2bb6c0848a613cdf3582eb9738
Martin Purita <martin.purita@olx.com>
2016-02-15 11:04:46 -0300

    Start refactor to a beautiful code

---
+++ un-dia-para-dar/src/main/java/itba/undiaparadar/utils/Constants.java
@@ -0,0 +1,32 @@
+package itba.undiaparadar.utils;
+
+/**
+ * This class contains all constants for th app
+ *
+ * @author Martin Purita - martinpurita@gmail.com
+ */
+public final class Constants {
+
+    /**
+     * This class contains all the constants values
+     */
+    public static class Value {
+        public static final int MIN_RADIUS = 100;
+    }
+
+    /**
+     * This class is use when you want to put/get an extra to a
+     * {@link android.os.Bundle} or {@link android.content.Intent}
+     */
+    public static class ExtraKeys {
+        public static final String TOPICS = "topics";
+        public static final String RADIUS = "radius";
+    }
+
+    /**
+     * This class contains all the result code for activities and fragments
+     */
+    public static class ResultCode {
+        public static final int FILTER = 1000;
+    }
+}
